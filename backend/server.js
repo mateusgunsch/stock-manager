311c808db089310e3e2d23d5e9c6b9dcf669edef
Mateus Gunsch <mateusgunsch01@gmail.com>
2025-03-18 14:49:41 -0400

    Tetando arrumar o CORS

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,11 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options("*", cors());
 app.use(express.json());
 app.use('/products', productRoutes);
 
