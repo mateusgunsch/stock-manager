d60c3c5980ad82f840355dcb0829d495e4d1aa74
Mateus Gunsch <mateusgunsch01@gmail.com>
2025-03-18 14:52:19 -0400

    Tetando arrumar o CORS .2

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,12 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.options("*", cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 app.use(express.json());
 app.use('/products', productRoutes);
 
